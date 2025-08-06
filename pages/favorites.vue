d9086483e0a9b5b3f6642a820895f7df13048ce3
murodismo <murodismo0609@gmail.com>
2025-08-06 14:45:46 +0500

    debugging favourites section

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,111 +1,99 @@
 <template>
-     <div 
-  v-if="!favoriteProducts.length" 
-  class="flex justify-center"
->
-  <div class="text-center">
-    <div class="bg-white p-6 md:p-4 rounded-lg shadow-md max-w-md mx-auto">
-      <img class="mb-10" src="/assets/hearts.png" alt="Error">
-      <h2 class="text-2xl font-semibold text-gray-800 mb-2">Sizning sevimlingiz bo'sh</h2>
-      <p class="text-gray-600 mb-6">
-        Mahsulotdagi
-        <UIcon name="material-symbols:favorite-outline-rounded" /> ni bosing , Akkauntingizga kiring va sevimliga saqlagan barcha mahsulotingiz saqlanib qoladi
-      </p>
-      <button 
-      @click="store.registerModal = !store.registerModal"
-        class="bg-[#feee00] cursor-pointer hover:bg-brand-green/80 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50"
-      >
-        Akkountga kirish
-      </button>
+  <div v-if="!favoriteProducts.length" class="flex justify-center">
+    <div class="text-center">
+      <div class="bg-white p-6 md:p-4 rounded-lg shadow-md max-w-md mx-auto">
+        <img class="mb-10" src="/assets/hearts.png" alt="Error">
+        <h2 class="text-2xl font-semibold text-gray-800 mb-2">Sizning sevimlingiz bo'sh</h2>
+        <p class="text-gray-600 mb-6">
+          Mahsulotdagi
+          <UIcon name="material-symbols:favorite-outline-rounded" /> ni bosing , Akkauntingizga kiring va sevimliga
+          saqlagan barcha mahsulotingiz saqlanib qoladi
+        </p>
+        <button @click="store.registerModal = !store.registerModal" v-if="!store.profileData" 
+          class="bg-[#feee00] cursor-pointer hover:bg-brand-green/80 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50">
+          Akkountga kirish
+        </button>
+        <button @click="router.push('/')" v-else 
+          class="bg-[#feee00] cursor-pointer hover:bg-brand-green/80 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50">
+          Bosh saxifaga o'tish
+        </button>
+      </div>
     </div>
   </div>
-</div>
 
-    <div v-if="favoriteProducts.length" class="container">
-        <h2 class="text-2xl font-bold my-6">Sevimli mahsulotlar soni: {{ store.profileData.data?.favorites?.favoriteProducts.length }}</h2>
-    </div>
-    <div v-if="favoriteProducts.length"  class="container grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6">
-        <div
-        @click="navigaItem(product.product.id , product.product.name)"
-        v-for="product in favoriteProducts"
-        :key="product.product.id"
-        class="bg-gray-200 relative card w-full max-w-[300px] h-[450px] cursor-pointer rounded-lg p-4 flex flex-col justify-between"
-      >
-      <UIcon @click.stop="deleteFavoritesHandler(store.profileData.data.favorites.id, product.product.id , product.product.name)" class="w-8 h-8 text-red-500 absolute right-2 z-50" name="mdi:heart" />
+  <div v-if="favoriteProducts.length" class="container">
+    <h2 class="text-2xl font-bold my-6">Sevimli mahsulotlar soni: {{
+      store.profileData.data?.favorites?.favoriteProducts.length }}</h2>
+  </div>
+  <div v-if="favoriteProducts.length" class="container grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6">
+    <div @click="navigaItem(product.product.id, product.product.name)" v-for="product in favoriteProducts"
+      :key="product.product.id"
+      class="bg-gray-200 relative card w-full max-w-[300px] h-[450px] cursor-pointer rounded-lg p-4 flex flex-col justify-between">
+      <UIcon
+        @click.stop="deleteFavoritesHandler(store.profileData.data.favorites.id, product.product.id, product.product.name)"
+        class="w-8 h-8 text-red-500 absolute right-2 z-50" name="mdi:heart" />
       <!-- <UIcon v-else  @click.stop="addFavorites(store.profileData.data.favorites.id, product.product.id , product.product.name)" class="w-8 h-8 absolute right-2 z-50" name="material-symbols-light:favorite-outline" /> -->
-        <!-- Rasm -->
-        <div class="flex justify-center items-center aspect-square mb-4">
-          <img
-            v-if="product.product.productImages?.length > 0"
-            :src="`https://api.albaraka.uz/${product.product.productImages[0].imageEntity.localImagePath}`"
-            :alt="`${product.product.name} - Image 1`"
-            class="w-full max-w-[260px] h-full max-h-[250px] object-contain rounded-lg transition-transform duration-300 ease-in-out hover:scale-110"
-          />
-        </div>
-      
-        <!-- Kontent (pastki qism) -->
-        <div class="flex flex-col justify-between flex-grow">
-          <div>
-            <h3 class="text-sm font-medium mb-3 line-clamp-2">{{ product.product.name }}</h3>
-      
-            <!-- To‘lov bo‘lsa -->
-            <div class="mb-3 flex justify-between gap-10 min-h-[32px]">
+      <!-- Rasm -->
+      <div class="flex justify-center items-center aspect-square mb-4">
+        <img v-if="product.product.productImages?.length > 0"
+          :src="`https://api.albaraka.uz/${product.product.productImages[0].imageEntity.localImagePath}`"
+          :alt="`${product.product.name} - Image 1`"
+          class="w-full max-w-[260px] h-full max-h-[250px] object-contain rounded-lg transition-transform duration-300 ease-in-out hover:scale-110" />
+      </div>
+
+      <!-- Kontent (pastki qism) -->
+      <div class="flex flex-col justify-between flex-grow">
+        <div>
+          <h3 class="text-sm font-medium mb-3 line-clamp-2">{{ product.product.name }}</h3>
+
+          <!-- To‘lov bo‘lsa -->
+          <div class="mb-3 flex justify-between gap-10 min-h-[32px]">
+            <div v-if="
+              product.product.productModel && product.product.marketResultmodel?.name === 'Uzum' && getParsedProductModel(product.product.productModel).Rating
+            " class="flex items-center gap-1">
+              <UIcon name="material-symbols:star-rounded" class="text-yellow-400 size-5" />
               <div
-                  v-if="
-                    product.product.productModel && product.product.marketResultmodel?.name === 'Uzum' && getParsedProductModel(product.product.productModel).Rating
-                  "
-                  class="flex items-center gap-1"
-                >
-                  <UIcon name="material-symbols:star-rounded" class="text-yellow-400 size-5" />
-                  <div
-        v-if="product.product.productModel && product.product.marketResultmodel && product.product.marketResultmodel.url === 'https://uzum.uz'"
-        class="text-sm font-bold"
-      >
-                    {{ getParsedProductModel(product.product.productModel).Rating }}
-                  </div>
-                </div>
-              <span
-                v-if="getParsedProductModel(product.product.productModel)?.SkuList?.[0]?.ProductOptionDtos?.[0]?.PaymentPerMonth && product.product.marketResultmodel?.url === 'https://uzum.uz'"
-                class="inline-block w-full text-center rounded-lg bg-green-100 text-green-800 py-1 text-sm font-medium"
-              >
-                {{
-                  getParsedProductModel(product.product.productModel).SkuList[0].ProductOptionDtos[0].PaymentPerMonth
-                }} so'm / oy
-              </span>
-      
-              <span
-                v-else-if="getParsedProductModel(product.product.productModel)?.storeProducts?.[0]?.monthly_repayment && product.product.marketResultmodel?.url === 'https://olcha.uz'"
-                class="inline-block w-full text-center rounded-lg bg-green-100 text-green-800 py-1 text-sm font-medium"
-              >
-                {{
-                  getParsedProductModel(product.product.productModel).storeProducts[0].monthly_repayment.toLocaleString('uz-UZ')
-                }} so'm / 12 months
-              </span>
-      
+                v-if="product.product.productModel && product.product.marketResultmodel && product.product.marketResultmodel.url === 'https://uzum.uz'"
+                class="text-sm font-bold">
+                {{ getParsedProductModel(product.product.productModel).Rating }}
+              </div>
             </div>
+            <span
+              v-if="getParsedProductModel(product.product.productModel)?.SkuList?.[0]?.ProductOptionDtos?.[0]?.PaymentPerMonth && product.product.marketResultmodel?.url === 'https://uzum.uz'"
+              class="inline-block w-full text-center rounded-lg bg-green-100 text-green-800 py-1 text-sm font-medium">
+              {{
+                getParsedProductModel(product.product.productModel).SkuList[0].ProductOptionDtos[0].PaymentPerMonth
+              }} so'm / oy
+            </span>
+
+            <span
+              v-else-if="getParsedProductModel(product.product.productModel)?.storeProducts?.[0]?.monthly_repayment && product.product.marketResultmodel?.url === 'https://olcha.uz'"
+              class="inline-block w-full text-center rounded-lg bg-green-100 text-green-800 py-1 text-sm font-medium">
+              {{
+                getParsedProductModel(product.product.productModel).storeProducts[0].monthly_repayment.toLocaleString('uz-UZ')
+              }} so'm / 12 months
+            </span>
+
           </div>
-      
-          <!-- Narx + Market -->
-          <div class="flex items-center justify-between mt-auto">
-            <div class="text-sm font-bold text-gray-800">
-              {{ product.product.price.toLocaleString('uz-UZ') }} so'm
-            </div>
-            <img
-              v-if="product.product.marketResultmodel?.url"
-              class="w-16"
-              :src="getMarketLogo(product.product.marketResultmodel.url)"
-              alt="Market Logo"
-            />
+        </div>
+
+        <!-- Narx + Market -->
+        <div class="flex items-center justify-between mt-auto">
+          <div class="text-sm font-bold text-gray-800">
+            {{ product.product.price.toLocaleString('uz-UZ') }} so'm
           </div>
+          <img v-if="product.product.marketResultmodel?.url" class="w-16"
+            :src="getMarketLogo(product.product.marketResultmodel.url)" alt="Market Logo" />
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useAuthStore } from '#imports';
 import { useRouter } from 'vue-router';
-import { ref , computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useToast } from '#imports'
 import successAudio from '@/assets/audio.mp3'
 import errorAudio from '@/assets/not-success.m4a'
@@ -157,7 +145,7 @@ async function addFavorites(
 
     if (response.ok) {
       store.getProfileData()
-        new Audio(successAudio).play();
+      new Audio(successAudio).play();
       toast.add({
         title: 'Muvaffaqiyatli',
         description: `${productName} sevimliga qo'shildi`,
@@ -166,7 +154,7 @@ async function addFavorites(
 
       // store.getProfileData() // Agar kerak bo‘lsa, import qilib ishlat
     } else {
-        new Audio(errorAudio).play();
+      new Audio(errorAudio).play();
       toast.add({
         title: 'Xatolik!',
         description: data?.message || 'Xatolik yuz berdi.',
@@ -175,7 +163,7 @@ async function addFavorites(
     }
   } catch (error: any) {
     console.error(error)
-        new Audio(errorAudio).play();
+    new Audio(errorAudio).play();
     toast.add({
       title: 'Xatolik!',
       description: error?.message || 'Internetni tekshiring.',
@@ -281,6 +269,7 @@ const getMarketLogo = (url: string): string => {
 .card {
   transition: box-shadow 0.2s ease;
 }
+
 .card:hover {
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
 }
@@ -291,5 +280,4 @@ const getMarketLogo = (url: string): string => {
   -webkit-box-orient: vertical;
   overflow: hidden;
 }
-
 </style>
